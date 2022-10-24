9cb60dfeca7f7494f870106c05774d5abb1ff4c6
AndrewLiis <andrewlis4774@gmail.com>
2022-10-24 19:35:42 +0200

    add button on menu screen

--- app/src/main/java/com/team/whackamole/ui/screens/MenuScreen.kt
+++ app/src/main/java/com/team/whackamole/ui/screens/MenuScreen.kt
@@ -1,19 +1,50 @@
 package com.team.whackamole.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.team.whackamole.R
 
 
 @Composable
 fun MenuScreen(navController: NavController) {
-    Column {
-        Text("Hello", style = TextStyle(fontSize = 22.sp))
-        Text("World", style = TextStyle(fontSize = 22.sp))
-        Text("from", style = TextStyle(fontSize = 22.sp))
-        Text("Jetpack Compose", style = TextStyle(fontSize = 22.sp))
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.ic_launcher_background),
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        Button(onClick = {
+            //Navigation on game screen
+        }, modifier = Modifier.padding(30.dp).fillMaxWidth()
+        ) {
+            Text("Начать игру", fontSize = 25.sp)
+        }
+        Button(onClick = {
+            //Navigation on store screen
+        }, modifier = Modifier.padding(30.dp, 15.dp).fillMaxWidth()
+        ) {
+            Text("Магазин", fontSize = 25.sp)
+        }
+        Button(onClick = {
+            //Navigation on settings screen
+        }, modifier = Modifier.padding(30.dp).fillMaxWidth()
+        ) {
+            Text("Настройки", fontSize = 25.sp)
+        }
     }
 }
