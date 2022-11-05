171d4ad47ec350974b9247beb74c714e1f4dfb55
AndrewLiis <andrewlis4774@gmail.com>
2022-11-05 12:40:15 +0200

    fixed design result screen

--- app/src/main/java/com/team/whackamole/ui/screens/ResultScreen.kt
+++ app/src/main/java/com/team/whackamole/ui/screens/ResultScreen.kt
@@ -1,14 +1,21 @@
 package com.team.whackamole.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.team.whackamole.R
+import com.team.whackamole.ui.Route
 
 @Composable
 fun ResultScreen(navController: NavController, score: String?) {
@@ -16,17 +23,37 @@ fun ResultScreen(navController: NavController, score: String?) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.background_forest),
+                contentScale = ContentScale.Crop
+            )
     ) {
         Text(
             text = "Конец игры!",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            color = Color.White,
             modifier = Modifier.fillMaxWidth()
         )
         Text(
-            text = "Вы набрали $score",
+            text = "Вы набрали $score очков",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            color = Color.White,
             modifier = Modifier.fillMaxWidth()
         )
-
+        Button(
+            onClick = { navController.navigate(Route.MenuScreenRoute.path) },
+            modifier = Modifier.padding(15.dp)
+        ) {
+            Text(
+                text = "Назад в меню",
+                fontSize = 30.sp,
+                color = Color.White,
+            )
+        }
     }
 
 }
