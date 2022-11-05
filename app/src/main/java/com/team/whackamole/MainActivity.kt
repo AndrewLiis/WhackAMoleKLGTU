9c83e66fad472b6015f4c694bf25713221a715ee
AndrewLiis <andrewlis4774@gmail.com>
2022-11-05 13:15:05 +0200

    hided all system ui

--- app/src/main/java/com/team/whackamole/MainActivity.kt
+++ app/src/main/java/com/team/whackamole/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.team.whackamole
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.team.whackamole.ui.navigation.NavigationController
@@ -8,6 +9,9 @@ import com.team.whackamole.ui.navigation.NavigationController
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
         setContent {
             NavigationController()
         }
